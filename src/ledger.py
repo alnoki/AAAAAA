0c2ef1bd87d1c963282a3adaa9cd6f3f1c3683d8
alnoki <43892045+alnoki@users.noreply.github.com>
2018-10-27 14:58:31 -0500

    Updates Transaction class input sanitation and associated test cases

--- src/ledger.py
+++ src/ledger.py
@@ -3,72 +3,81 @@ This module includes classes and functions related to ledger-style
 data.
 """
 import datetime
+from typing import Union
+
+transact_types = set(['Bank transfer', 'Fees', 'Buy', 'Sell', 'Dividends'])
 
 
 class Transaction:
-    """Represents a bank transfer, buy, dividends, fee, or sell
-    """
-    def __init__(self, date: datetime.date, total_amount: float,
-                 transact_type: str, symbol: str = None,
-                 num_shares: int = None, description: str = None):
-
-        # Sanitation of required inputs
-        if type(date) is not datetime.date:
-            raise ValueError(f"{self.__class__.__name__} date")
+    """Represents a bank transfer, buy, dividends, fee, or sell"""
+    def __init__(self, date: datetime.date, total_amount: Union[float, int],
+                 transact_type: str, symbol: str=None,
+                 num_shares: int=None, description: str=None):
+        # Check that positional arguments are correct data type
+        if (not isinstance(date, datetime.date) or
+                not isinstance(total_amount, (int, float)) or
+                not isinstance(transact_type, str)):
+            raise AttributeError
+            return
+        # Check that positional arguments have reasonable data
+        if (total_amount < 0 or
+                transact_type not in transact_types):
+            raise ValueError
+            return
         self.date = date
+        self.total_amount = float(total_amount)
+        self.transact_type = transact_type
         self.symbol = symbol
         self.num_shares = num_shares
-        self.total_amount = total_amount
-        self.transact_type = transact_type
         self.description = description
 
-    @property
-    def per_share_amount(self):
-        # Can't compute if data is missing and don't want divide by 0
-        if self.num_shares is None or self.total_amount is None \
-                or self.num_shares == 0:
-            return None
-        return self.total_amount / self.num_shares
+# @property
+# def per_share_amount(self):
+#     # Can't compute if data is missing and don't want divide by 0
+#     if self.num_shares is None or self.total_amount is None \
+#             or self.num_shares == 0:
+#         return None
+#     return self.total_amount / self.num_shares
 
-    def __str__(self):
-        return f"{self.date}, {self.symbol}, {self.num_shares}, " \
-               f"{self.per_share_amount}, {self.total_amount}, " \
-               f"{self.transact_type}, {self.description}"
+# def __str__(self):
+#     return f"{self.date}, {self.symbol}, {self.num_shares}, " \
+#            f"{self.per_share_amount}, {self.total_amount}, " \
+#            f"{self.transact_type}, {self.description}"
 
-    def __repr__(self):
-        # Per share amount is not needed to reproduce object
-        return f"{self.__class__.__name__}({self.date}, {self.symbol}, " \
-               f"{self.num_shares}, " \
-               f"{self.total_amount}, {self.transact_type}, {self.description})"
+# def __repr__(self):
+#     # Per share amount is not needed to reproduce object
+#     return f"{self.__class__.__name__}({self.date}, {self.symbol}, " \
+#            f"{self.num_shares}, " \
+#            f"{self.total_amount}, {self.transact_type}, {self.description})"
 
-    # @classmethod
-    # def from_string(cls, string):
-    #     """
-    #     Sanitizes and re-casts arguments from a string defining a
-    #     transaction. Strips off class definition and associated ()
-    #     :param string: Of form:
-    #     "Transaction(2018-10-12, AAPL, 25, 4000, Buy, Apple buy)"
-    #     :return: A Transaction object
-    #     """
-    #     import re
-    #     # Regex looks for class name, then left paren, then any number
-    #     # of non new-line chars, then right parent before end of string
-    #     pattern = re.compile(f"{cls.__name__}\((.+)\)$")
-    #     match = pattern.findall(string)  # Could probably be different
-    #     if match is None:
-    #         return None
-    #     date, symbol, num_shares, total_amount, transact_type, description = \
-    #         match[0].split(', ') # This could probably be cleaner
-    #     # Recast relevant attributes
-    #     date = None if date == 'None' else \
-    #         datetime.datetime.strptime(date, '%Y-%m-%d').date()
-    #     symbol = None if symbol == 'None' else symbol
-    #     num_shares = None if num_shares == 'None' else int(num_shares)
-    #     total_amount = None if total_amount == 'None' else float(total_amount)
-    #     transact_type = None if transact_type == 'None' else transact_type
-    #     description = None if description == 'None' else description
-    #     return(cls, date, symbol, num_shares, total_amount, transact_type,
-    #            description)
+# @classmethod
+# def from_string(cls, string):
+#     """
+#     Sanitizes and re-casts arguments from a string defining a
+#     transaction. Strips off class definition and associated ()
+#     :param string: Of form:
+#     "Transaction(2018-10-12, AAPL, 25, 4000, Buy, Apple buy)"
+#     :return: A Transaction object
+#     """
+#     import re
+#     # Regex looks for class name, then left paren, then any number
+#     # of non new-line chars, then right parent before end of string
+#     pattern = re.compile(f"{cls.__name__}\((.+)\)$")
+#     match = pattern.findall(string)  # Could probably be different
+#     if match is None:
+#         return None
+#     date, symbol, num_shares, total_amount, transact_type, description = \
+#         match[0].split(', ') # This could probably be cleaner
+#     # Recast relevant attributes
+#     date = None if date == 'None' else \
+#         datetime.datetime.strptime(date, '%Y-%m-%d').date()
+#     symbol = None if symbol == 'None' else symbol
+#     num_shares = None if num_shares == 'None' else int(num_shares)
+#     total_amount = None if total_amount == 'None' else float(total_amount)
+#     transact_type = None if transact_type == 'None' else transact_type
+#     description = None if description == 'None' else description
+#     return(cls, date, symbol, num_shares, total_amount, transact_type,
+#            description)
 
 
 # def test_section(description):
@@ -83,7 +92,8 @@ class Transaction:
 # transaction = Transaction(datetime.date(2018, 10, 12), 'AAPL', 20, 4000,
 #                           'Buy', "Apple market buy")
 # bank_transaction = Transaction(datetime.date(2019, 6, 9), None, 500,
-#                                "Bank transfer", "Transfer to brokerage account")
+#                                "Bank transfer", "Transfer to brokerage\
+# account")
 # print(transaction.per_share_amount)  # Nominal case
 # transaction.num_shares = None
 # print(transaction.per_share_amount)  # num_shares is None
--- test/test_ledger.py
+++ test/test_ledger.py
@@ -1,60 +1,69 @@
 import datetime
-from src.ledger import Transaction
+from src.ledger import Transaction, transact_types
 import pytest
 
 
-class TestInit(object):
+class TestTransactionInit(object):
     """Tests the initialization functions of the Transaction class"""
 
-    # Default values from required arguments
+    # Default values for required arguments
     d_date = datetime.date.today()
     d_total_amount = 0
     d_transact_type = 'Buy'
 
     @pytest.mark.parametrize(
-        "in_var", [None, 1, 'abc', datetime.date(2018, 10, 12),
+        "i_date", [None, 1, 'abc', datetime.date(2018, 10, 12),
                    datetime.date(1, 1, 1)])
-    def test_date(self, in_var):
-        """Verifies the date is correctly initialized"""
-        if type(in_var) is not datetime.date:
+    def test_date(self, i_date):
+        """Verifies date is correctly initialized"""
+        # Attribute error should be raised if date isn't correct type
+        if not isinstance(i_date, datetime.date):
+            with pytest.raises(AttributeError):
+                Transaction(date=i_date, total_amount=self.d_total_amount,
+                            transact_type=self.d_transact_type)
+        else:  # Check date has been assigned correctly
+            t = Transaction(date=i_date, total_amount=self.d_total_amount,
+                            transact_type=self.d_transact_type)
+            assert t.date == i_date
+
+    @pytest.mark.parametrize("i_total_amount", [None, -2, 'abc', 45, 45.0])
+    def test_total_amount(self, i_total_amount):
+        """Verifies total_amount is correctly initialized"""
+        # Attribute error should be raised if not a float or an int
+        if not isinstance(i_total_amount, (float, int)):
+            with pytest.raises(AttributeError):
+                Transaction(date=self.d_date, total_amount=i_total_amount,
+                            transact_type=self.d_transact_type)
+        # Value error should be raised if amount is negative
+        elif i_total_amount < 0:
             with pytest.raises(ValueError):
-                Transaction(date=in_var, total_amount=self.d_total_amount,
+                Transaction(date=self.d_date, total_amount=i_total_amount,
                             transact_type=self.d_transact_type)
-        else:
-            t = Transaction(date=in_var, total_amount=self.d_total_amount,
+        else:  # Verify value has been cast as float and assigned
+            t = Transaction(date=self.d_date, total_amount=i_total_amount,
                             transact_type=self.d_transact_type)
-            assert t.date == in_var
+            assert (t.total_amount == i_total_amount and
+                    type(t.total_amount) is float)
 
-    # @pytest.mark.parametrize(
-    #     "in_var", [None, '', ' ', 'AAPL', 'BRK.A', 'AAPL', '^CRSPTMT'])
-    # def test_symbol(self, in_var):
-    #     """Verifies the symbol is correctly initialized"""
-    #     t = Transaction(symbol=in_var)
-    #     assert t.symbol == in_var
-    #
-    # @pytest.mark.parametrize(
-    #     "in_var", [None, 0, 1.2, 3, -1])
-    # def test_num_shares(self, in_var):
-    #     """Verifies the number of shares is correctly initialized"""
-    #     t = Transaction(num_shares=in_var)
-    #     assert t.num_shares == in_var
+    @pytest.mark.parametrize("i_transact_type",
+                             [None, 3, '', ' ', 'buy', 'Sell'])
+    def test_transact_type(self, i_transact_type):
+        """Verifies transact_type is correctly initialized"""
+        # Attribute error should be raised if type is not string
+        if not isinstance(i_transact_type, str):
+            with pytest.raises(AttributeError):
+                Transaction(date=self.d_date, total_amount=self.d_total_amount,
+                            transact_type=i_transact_type)
+        # Value error should be raised if isn't 'Buy', 'Sell', etc.
+        # Case sensitive: 'buy' should fail, but 'Buy' would pass
+        elif i_transact_type not in transact_types:
+            with pytest.raises(ValueError):
+                Transaction(date=self.d_date, total_amount=self.d_total_amount,
+                            transact_type=i_transact_type)
+        else:  # Verify value has been assigned correctly
+            t = Transaction(date=self.d_date, total_amount=self.d_total_amount,
+                            transact_type=i_transact_type)
+            assert t.transact_type == i_transact_type
 
-
-
-# class TestTransaction(unittest.TestCase):
-#     """Test case for validating Transaction class"""
-#
-#     def setUp(self):
-#         """Initializes new class instances for each test"""
-#         self.t0 = Transaction(datetime.date(2018, 10, 12), 'AAPL', 20, 4000,
-#                               'Buy', "Apple market buy")
-#         self.t1 = Transaction(datetime.date(2019, 6, 9), None, None, 500,
-#                               "Bank transfer", "Transfer from Chase")
-#
-#     def test_init(self):
-#         """Tests that class attributes are stored as expected"""
-#         self.assertEqual(self.t0.date, datetime.date(2018, 10, 12))
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+# Can't have num_shares if symbol is None....
+# Can make sure that transaction type is associated with a ticker if applicable
